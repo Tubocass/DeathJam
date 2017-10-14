e2ebe2c8ddd42c0f1856045275943250bed2768a
BoB Tubocass <tubocass@gmail.com>
2017-10-14 13:56:54 -0500

    Initial commit. Gun points and shoots correctly.

---
+++ DeathJam/Assets/DeathJam/Scripts/GunController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour, IWeapon {
+
+	[SerializeField]GameObject bulletFab;
+	[SerializeField]Transform Muzzle;
+	[SerializeField] int clipSize = 10;
+	GameObject[] bulletPool;
+	bool canFire = true;
+
+	void OnEnable()
+	{
+		bulletPool = new GameObject[clipSize];
+		for(int b = 0; b<bulletPool.Length; b++)
+		{
+			bulletPool[b] = (GameObject)Instantiate(bulletFab,Muzzle.position,Quaternion.identity);
+			bulletPool[b].SetActive(false);
+		}
+	}
+	public void Attack(Vector3 Position)
+	{
+		if(canFire)
+		{
+			//Quaternion q = Quaternion.LookRotation(Direction - Muzzle.position, Vector3.forward);
+			for(int b = 0; b<bulletPool.Length; b++)
+			{
+				if(!bulletPool[b].activeSelf)
+				{
+					bulletPool[b].transform.position = Muzzle.position;
+					bulletPool[b].transform.rotation = this.transform.rotation;
+					bulletPool[b].SetActive(true);
+					//bulletPool[b].transform.LookAt(Direction,Vector3.forward);//Rotate(Vector3.forward,Vector3.Angle(transform.up,Direction));
+//					Quaternion rot = Quaternion.LookRotation (transform.position - Position, Vector3.forward);
+//					bulletPool[b].transform.rotation = rot;
+//					bulletPool[b].transform.eulerAngles = new Vector3 (0, 0, bulletPool[b].transform.eulerAngles.z);
+					//GetComponent<Rigidbody2D>().angularVelocity = 0;
+					break;
+				}
+			}
+			canFire = false;
+			StartCoroutine(Cooldown());
+		}
+	}
+
+	IEnumerator Cooldown()
+	{
+		yield return new WaitForSeconds(0.25f);
+		canFire = true;
+	}
+}
---
+++ DeathJam/Assets/DeathJam/Scripts/InputControls.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControls : MonoBehaviour 
+{
+
+	[SerializeField]float moveSpeed;// minFOV, maxFOV, scrollSpeed;
+	[SerializeField]GameObject weapon;
+	IWeapon currentWeapon;
+	Vector3 movement;
+	[SerializeField]LayerMask mask;
+	Transform tran;
+	// Update is called once per frame
+	void Start()
+	{
+		//mask = 1<<LayerMask.NameToLayer("Ground");
+		tran = transform;
+		if(weapon!=null)
+		{
+			currentWeapon = weapon.GetComponent<IWeapon>();
+		}
+	}
+	void Update () 
+	{
+		float lastInputX = Input.GetAxis ("Horizontal");
+		float lastInputY = Input.GetAxis ("Vertical");
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+//		float lastInputScroll = Input.GetAxis("Mouse ScrollWheel");
+//				if(lastInputScroll>0f || lastInputScroll<0f)
+//		{
+//			Camera.main.orthographicSize -= lastInputScroll* scrollSpeed;
+//			Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minFOV, maxFOV);
+//		}
+
+		if(lastInputX != 0f || lastInputY != 0f)
+		{
+			movement = new Vector3(lastInputX, lastInputY, 0);
+			Vector3 targetDir  = transform.position + movement * moveSpeed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position,targetDir,1f);
+		}
+
+		if (Input.GetMouseButton (0)) 
+		{
+			//currentWeapon.Attack(dir);
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 20f, mask)) 
+			{
+				Vector3 dir = hit.point-transform.position;
+				currentWeapon.Attack(dir);
+			}
+		}
+		if(currentWeapon!=null)
+		{
+			Quaternion rot = Quaternion.LookRotation (weapon.transform.position - mousePos, Vector3.forward);
+			weapon.transform.rotation = rot;
+			weapon.transform.eulerAngles = new Vector3 (0, 0, weapon.transform.eulerAngles.z+90);
+			if(Vector3.Dot(mousePos-transform.position,Vector3.right)>0)//mouse is to the right of us
+			{
+				Debug.Log("I Was RIGHT");
+				//weapon.GetComponent<SpriteRenderer>().flipX = false;
+				weapon.GetComponent<SpriteRenderer>().flipY = false;
+
+			}else
+			{
+				//weapon.GetComponent<SpriteRenderer>().flipX = true;
+				weapon.GetComponent<SpriteRenderer>().flipY = true;
+			}
+//			if(Vector3.Angle(mousePos-transform.position,weapon.transform.right)>1)
+//			weapon.transform.Rotate(Vector3.forward, Vector3.Angle(mousePos-transform.position,weapon.transform.right));
+			//weapon.transform.LookAt(mousePos);
+		}
+	}
+
+}
